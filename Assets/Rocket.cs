80978aa5e77a59364c3b780505539e92254ffb70
MakerMush <71267581+MakerMush@users.noreply.github.com>
2020-10-20 09:40:22 -0400

    Lesson 15 (added audio)

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,12 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource rocketSound;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        rocketSound = GetComponent<AudioSource>();
         
     }
 
@@ -23,15 +25,22 @@ public class Rocket : MonoBehaviour
             print("Thrusting");
             // time to fly!
             rigidBody.AddRelativeForce(Vector3.up);
-            
+            // make some noiiiiise
+            if (!rocketSound.isPlaying){
+                rocketSound.Play();
+            }
+        } else {
+            rocketSound.Stop();
         }
         
         if (Input.GetKey(KeyCode.A))
         {
+            transform.Rotate(Vector3.forward);
             print("A pressed!");
         } 
         else if (Input.GetKey(KeyCode.D))
         {
+            transform.Rotate(-Vector3.forward);
             print("D pressed!");
         }
 
